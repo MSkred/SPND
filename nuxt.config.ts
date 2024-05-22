65c1a49094a39a4a0ff2e458536faaf4847cb864
MSkred <maxime.scibetta@outlook.com>
2024-05-22 11:36:27 +0800

    Added google-fonts package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxthub/core', "@nuxt/eslint", "@nuxt/ui"],
+  modules: ['@nuxthub/core', "@nuxt/eslint", "@nuxt/ui", "@nuxtjs/google-fonts"],
   hub: {
     database: true,
   },
@@ -10,5 +10,11 @@ export default defineNuxtConfig({
       // Enable Server API documentation within NuxtHub
       openAPI: true
     }
-  }
+  },
+  googleFonts: {
+    families: {
+      Roboto: [100, 300, 400, 500, 700, 900],
+      'Poetsen One': true
+    }
+  },
 })
