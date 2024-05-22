db2a66f46e659c1c36b469358f9bcfa09d27147f
MSkred <maxime.scibetta@outlook.com>
2024-05-22 16:36:45 +0800

    Delete unused files and useles consoleLog

--- middleware/loggedIn.global.ts
+++ middleware/loggedIn.global.ts
@@ -1,5 +1,5 @@
 export default defineNuxtRouteMiddleware((to, from) => {
-console.log('MIDDLE', );
+
     const { loggedIn } = useUserSession()
    
     if (!loggedIn.value && to.name !== 'login') {
