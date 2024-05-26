35666e32a4870fb31cd5fd1effbcc912c8db4bc7
MSkred <maxime.scibetta@outlook.com>
2024-05-26 18:28:19 +0800

    feat: beautify board view

--- pages/board.vue
+++ pages/board.vue
@@ -3,90 +3,34 @@
     <UDashboardPanel grow>
       <UDashboardNavbar title="Tableaux" :badge="boards.length">
         <template #right>
-          <UInput
-            ref="input"
-            v-model="q"
-            icon="i-heroicons-funnel"
-            autocomplete="off"
-            placeholder="Filtrer les tableaux"
-            class="hidden lg:block"
-            @keydown.esc="$event.target.blur()"
-          >
+          <UInput ref="input" v-model="q" icon="i-heroicons-funnel" autocomplete="off" placeholder="Filtrer les tableaux" class="hidden lg:block" @keydown.esc="$event.target.blur()">
             <template #trailing>
               <UKbd value="/" />
             </template>
           </UInput>
 
-          <UButton
-            label="Nouveau tableau"
-            trailing-icon="i-heroicons-plus"
-            color="gray"
-            @click="createModalOpen = true"
-          />
+          <UButton label="Nouveau tableau" trailing-icon="i-heroicons-plus" color="gray" @click="createModalOpen = true"/>
         </template>
       </UDashboardNavbar>
 
-      <UDashboardModal
-        v-model="createModalOpen"
-        title="Nouveau tableau"
-        description="Créer une nouveau tableau dans votre système"
-        :ui="{ width: 'sm:max-w-md' }"
-      >
+      <UDashboardModal v-model="createModalOpen" title="Nouveau tableau" description="Créer une nouveau tableau dans votre système" :ui="{ width: 'sm:max-w-md' }">
         <BoardCreateForm @close="onFormClose()" />
       </UDashboardModal>
 
-      <UDashboardModal
-        v-model="updateModalOpen"
-        title="Modification du tableau"
-        :ui="{ width: 'sm:max-w-md' }"
-      >
-        <BoardEditForm
-          v-if="currentBoard"
-          :board="currentBoard"
-          @close="onFormClose()"
-        />
+      <UDashboardModal v-model="updateModalOpen" title="Modification du tableau" :ui="{ width: 'sm:max-w-md' }">
+        <BoardEditForm v-if="currentBoard" :board="currentBoard" @close="onFormClose()"/>
       </UDashboardModal>
 
-      <UDashboardModal
-        v-if="currentBoard"
-        v-model="deleteModalOpen"
-        :title="`Suppression du tableau : ${currentBoard.name}`"
-        :description="`Êtes-vous sûr de vouloir suprimer le tableau : ${currentBoard.name} ?`"
-        icon="i-heroicons-exclamation-circle"
-        :ui="{
-        icon: { base: 'text-red-500 dark:text-red-400' } as any,
-        footer: { base: 'ml-16' } as any
-      }"
-      >
+      <UDashboardModal v-if="currentBoard" v-model="deleteModalOpen" :title="`Suppression du tableau : ${currentBoard.name}`" :description="`Êtes-vous sûr de vouloir suprimer le tableau : ${currentBoard.name} ?`" icon="i-heroicons-exclamation-circle" :ui="{ icon: { base: 'text-red-500 dark:text-red-400' } as any, footer: { base: 'ml-16' } as any}">
         <template #footer>
-          <UButton
-            color="red"
-            label="Supprimer"
-            :loading="loading"
-            @click="onDelete"
-          />
-          <UButton
-            color="white"
-            label="Annuler"
-            @click="deleteModalOpen = false"
-          />
+          <UButton color="red" label="Supprimer" :loading="loading" @click="onDelete"/>
+          <UButton color="white" label="Annuler" @click="deleteModalOpen = false"/>
         </template>
       </UDashboardModal>
 
       <UDashboardToolbar>
-        <!-- <template #left>
-          <USelectMenu v-model="selectedStatuses" icon="i-heroicons-cog-8-tooth" placeholder="Groupe" multiple
-            :options="defaultStatuses" :ui-menu="{ option: { base: 'capitalize' } }" />
-          <USelectMenu v-model="selectedLocations" icon="i-heroicons-table-cells-20-solid" placeholder="Tableau"
-            :options="defaultLocations" multiple />
-        </template> -->
         <template #right>
-          <USelectMenu
-            v-model="selectedColumns"
-            icon="i-heroicons-adjustments-horizontal-solid"
-            :options="defaultColumns"
-            multiple
-          >
+          <USelectMenu v-model="selectedColumns" icon="i-heroicons-adjustments-horizontal-solid" :options="defaultColumns" multiple>
             <template #label> Affichage </template>
           </USelectMenu>
         </template>
@@ -95,31 +39,30 @@
         <template #empty-state>
           <div class="flex flex-col items-center justify-center py-6 gap-3">
             <span class="text-sm">Aucun tableau pour ce groupe</span>
-            <UButton
-              label="Créer un tableau"
-              trailing-icon="i-heroicons-plus"
-              @click="createModalOpen = true"
-            />
+            <UButton label="Créer un tableau" trailing-icon="i-heroicons-plus" @click="createModalOpen = true"/>
           </div>
         </template>
-        <template #icon-data="{ row }">
-          <span>{{ row.icon ? row.icon : "-" }}</span>
+        <template #name-data="{ row }">
+          <span class="rounded text-white px-1.5 py-0.5" :style="{ 'background-color': row.color }">{{ row.icon ? row.icon + ' ' : '' }}{{ row.name }}</span>
         </template>
-        <template #color-data="{ row }">
-          <div
-            v-if="row.color"
-            class="h-3 w-3 rounded"
-            :style="{ 'background-color': row.color }"
-          ></div>
-          <template v-else> - </template>
+        <template #income-data="{ row }">
+          <span>{{ row.income ? row.income : '-'}}</span>
+        </template>
+        <template #startDate-data="{ row }">
+          <span>{{ format(row.startDate, 'd MMM, yyy')}}</span>
+        </template>
+        <template #endDate-data="{ row }">
+          <span>{{ row.endDate ? format(row.endDate, 'd MMM, yyy') : '-'}}</span>
+        </template>
+        <template #createdAt-data="{ row }">
+          <span>{{ format(row.createdAt, 'dd/MM/yy, hh:mm')}}</span>
+        </template>
+        <template #updatedAt-data="{ row }">
+          <span>{{ format(row.updatedAt, 'dd/MM/yy, hh:mm')}}</span>
         </template>
         <template #actions-data="{ row }">
           <UDropdown :items="items(row)">
-            <UButton
-              color="gray"
-              variant="ghost"
-              icon="i-heroicons-ellipsis-horizontal-20-solid"
-            />
+            <UButton color="gray" variant="ghost" icon="i-heroicons-ellipsis-horizontal-20-solid"/>
           </UDropdown>
         </template>
       </UTable>
@@ -128,19 +71,17 @@
 </template>
 
 <script setup lang="ts">
+import { format, setDefaultOptions } from "date-fns";
+import { fr } from "date-fns/locale";
+setDefaultOptions({ locale: fr });
 import { type Board } from "~/server/utils/drizzle";
 
 const route = useRoute();
 const q = ref("");
-useSeoMeta({
-  title: "Tableaux",
-});
+useSeoMeta({ title: "Tableaux", });
 // Table columns
 const defaultColumns = [
-  { key: "id", label: "ID" },
   { key: "name", label: "Nom" },
-  { key: "icon", label: "Icône" },
-  { key: "color", label: "Couleur" },
   { key: "currencyIsoCode", label: "Devise" },
   { key: "income", label: "Entrée" },
   { key: "objective", label: "Objectif" },
@@ -158,22 +99,18 @@ const columns = computed(() =>
 // Tables actions row
 const items = (row: Board) => {
   let items = [
-    [
-      {
-        label: "Editer",
-        icon: "i-heroicons-pencil-square-20-solid",
-        click: () => {
-          currentBoard.value = row;
-          updateModalOpen.value = true;
-        },
+    [{
+      label: "Editer",
+      icon: "i-heroicons-pencil-square-20-solid",
+      click: () => {
+        currentBoard.value = row;
+        updateModalOpen.value = true;
       },
-    ],
-    [
-      {
-        label: "Accéder",
-        icon: "i-heroicons-arrow-right-circle-20-solid",
-      },
-    ],
+    },],
+    [{
+      label: "Accéder",
+      icon: "i-heroicons-arrow-right-circle-20-solid",
+    },],
   ];
   if (!row.private) {
     items.push([
@@ -191,16 +128,7 @@ const items = (row: Board) => {
 };
 // Table data
 const groupId = ref(route.query.group)
-const {
-  data: boards,
-  refresh,
-  pending,
-} = await useFetch<Board[]>(`/api/boards`, {
-  query: { group: groupId },
-  deep: false,
-  lazy: true,
-  default: () => [],
-});
+const { data: boards, refresh, pending } = await useFetch<Board[]>(`/api/boards`, { query: { group: groupId }, deep: false, lazy: true, default: () => [] });
 function onFormClose() {
   createModalOpen.value = false;
   updateModalOpen.value = false;
@@ -219,24 +147,13 @@ const toast = useToast();
 async function onDelete() {
   loading.value = true;
   try {
-    await $fetch(`/api/boards/${currentBoard.value.id}`, {
-      method: "DELETE",
-    });
-    toast.add({
-      icon: "i-heroicons-check-circle",
-      title: `Le tableau "${currentBoard.value.name}" a bien été supprimé.`,
-      color: "green",
-    });
+    await $fetch(`/api/boards/${currentBoard.value.id}`, { method: "DELETE" });
+    toast.add({ icon: "i-heroicons-check-circle", title: `Le tableau "${currentBoard.value.name}" a bien été supprimé.`, color: "green" });
     onFormClose();
   } catch (e) {
     if (e instanceof Error) {
       console.error(e);
-      toast.add({
-        icon: "i-heroicons-exclamation-circle",
-        title: "Veuillez réessayer",
-        description: e.message,
-        color: "red",
-      });
+      toast.add({ icon: "i-heroicons-exclamation-circle", title: "Veuillez réessayer", description: e.message, color: "red" });
       loading.value = false;
     }
   }
